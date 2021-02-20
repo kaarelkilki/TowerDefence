a8735a8c3cd0c1fa3e806eeedd264acac03a816c
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2021-02-20 15:29:16 +0200

    ExploreNeighbours

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -8,11 +8,27 @@ public class Pathfinder : MonoBehaviour
     [SerializeField] Waypoint startWaypoint, endWaypoint;
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
+    Vector2Int[] directions = {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            print(direction);
+        }
     }
 
     private void ColorStartAndEnd()
