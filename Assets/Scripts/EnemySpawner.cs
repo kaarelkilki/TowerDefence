ff0f50528a3c82d524321e40bebcb1742cb292d0
kaarelkilki <65207477+kaarelkilki@users.noreply.github.com>
2021-02-25 05:35:34 +0200

    EnemySpawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [Range(0.1f, 120f)]
     [SerializeField] float secondsBetweenSpawns = 2f;
     [SerializeField] EnemyMovement enemyPrefab;
     // Start is called before the first frame update
@@ -16,7 +17,7 @@ public class EnemySpawner : MonoBehaviour
     {
         while (true)// forever
         {
-            print("Spawning enemy");
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
             yield return new WaitForSeconds(secondsBetweenSpawns);
         }
     }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+    [Header("Parameters of the Tower")]
     [SerializeField] Transform objectToPan;
-    [SerializeField] Transform targetEnemy;
     [SerializeField] float attackRange = 20f;
     [SerializeField] ParticleSystem projectileParticle;
 
+    [Header("State of the Tower")]
+    [SerializeField] Transform targetEnemy;
     // Update is called once per frame
     void Update()
     {
+        SetTargetEnemy();
         if (targetEnemy)
         {
             objectToPan.LookAt(targetEnemy);
@@ -22,6 +25,35 @@ public class Tower : MonoBehaviour
         {
             Shoot(false);
         }
+    }
+
+    private void SetTargetEnemy()
+    {
+        var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+        if (sceneEnemies.Length == 0) { return; };
+
+        Transform closestEnemy = sceneEnemies[0].transform;
+
+        foreach (EnemyDamage testEnemy in sceneEnemies)
+        {
+            closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+        }
+
+        targetEnemy = closestEnemy;
+    }
+
+    private Transform GetClosest(Transform transformA, Transform transformB)
+    {
+        var distToA = Vector3.Distance(transform.position, transformA.position);
+        var distToB = Vector3.Distance(transform.position, transformB.position);
+
+        if (distToA < distToB)
+        {
+            return transformA;
+        }
+
+        return transformB;
+
     }
 
     private void FireAtEnemy()
